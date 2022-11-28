deadd5f0d77914138907924cc9ec69b8d390b046
Mayuresh123 <56029669+mayuresh714@users.noreply.github.com>
2022-11-28 18:56:17 +0530

    Add files via upload

---
+++ filesearch1.0.0/basic_search/demo.py
@@ -0,0 +1,58 @@
+
+
+from  search import Filemanager
+from  queryDB import engine_db as edb
+
+
+class Fusion:
+    def __init__(self):
+        self.edb = edb() 
+        self.fmng = None
+
+    # def invert_every_file(s):
+    #     for i in s.fmng.db.iloc[:,:].index:
+    #         s.inv_ind.inverting_file( s.fmng.db.loc[i,"file_name"] , s.fmng.db.loc[i,"id"]  )
+
+    def index_files(self,path = "c:/users/hp/desktop" ):
+        self.fmng = Filemanager( path )
+        self.fmng.DFS_method(path)
+
+    def print_result(self,query ):  
+        try:          
+            # result_set = s.inv_ind.fetch_result(query)
+            print("----------------------requiresd files-------------------")
+            for ind,row in enumerate(self.edb.search_filename(query)):
+                print( ind ,"=>", row[-2] )
+            print("--------------------------------------------------------")
+        except:
+            print(f"no file named as {query}")
+
+
+    # def total_index_creation(s,path):
+    #     s.fmng = Filemanager(path)
+    #     s.fmng.process()
+    #     return 1
+
+    def run_search_demo(self):
+        print("welcome to file search v1.0.0..")
+        inp = input("\nchoose any of the following action:\n1.add folder path\n2.search\n3.end\n")
+        while 1:
+            if inp == "1":
+                path = input("\nenter your folder path: ")
+                self.index_files(path)
+                print("\nindex creation successful .. :)")
+            elif inp == "2":
+                query = input("\nenter the search query: ")
+                self.print_result(query)
+            elif inp == "3":
+                print("\nthank you ....*/-\*")
+                break 
+            
+            inp = input("\nchoose any of the following action:\n1.add folder path\n2.search\n3.end\n")
+    
+if __name__ == "__main__":
+    obj = Fusion()
+    obj.run_search_demo()
+    
+     
+
